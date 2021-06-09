1dca8474076d85d1e2ef192d0144dee0e5023dbf
ArjunBhalla98 <arjunbhallasg@gmail.com>
2021-06-09 11:27:30 -0400

    print

--- train.py
+++ train.py
@@ -139,7 +139,7 @@ if __name__ == "__main__":
             #### PUT MODEL SPECIFIC FORWARD PASS CODE HERE ####
             print(data["img_paths"])
             img, mask = factorspeople.get_image(
-                data["img_paths"].pop(), data["mask_paths"].pop()
+                data["img_paths"].pop()[0], data["mask_paths"].pop()[0]
             )
             gt = img.detach() * mask.detach()
             out = factorspeople.reconstruct(img, mask)
