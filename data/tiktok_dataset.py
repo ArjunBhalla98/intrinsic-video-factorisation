f7e171ad8bb7c17cc8f5b9597331583d7d3cd108
ArjunBhalla98 <arjunbhallasg@gmail.com>
2021-06-09 19:01:03 -0400

    remove problem files

--- data/tiktok_dataset.py
+++ data/tiktok_dataset.py
@@ -42,7 +42,7 @@ class TikTokDataset(Dataset):
         appended_path = root_dir
 
         for folder in os.listdir(appended_path)[:40]:
-            if not os.path.isdir(appended_path + folder):
+            if not os.path.isdir(appended_path + folder) or "234" in folder:
                 continue
 
             video_imgs = glob.glob(f"{appended_path}/{folder}/images/*.png")
