b18d3d08debdc472101c53334f668391ae69e704
ArjunBhalla98 <arjunbhallasg@gmail.com>
2021-06-29 15:05:51 -0400

    RH code

--- train.py
+++ train.py
@@ -92,47 +92,48 @@ if __name__ == "__main__":
         device,
         transform=transform,
         sample_size=BATCH_SIZE,
-        # squarize_size=1024,
+        squarize_size=1024,
     )
-    dataset_test = TikTokDataset(
-        ROOT_DIR,
-        device,
-        train=False,
-        transform=transform,
-        sample_size=BATCH_SIZE,
-        # squarize_size=1024,
-    )
+    # dataset_test = TikTokDataset(
+    #     ROOT_DIR,
+    #     device,
+    #     train=False,
+    #     transform=transform,
+    #     sample_size=BATCH_SIZE,
+    #     squarize_size=1024,
+    # )
     train_loader = DataLoader(dataset_train, shuffle=True)
-    test_loader = DataLoader(dataset_test, shuffle=True)
+    # test_loader = DataLoader(dataset_test, shuffle=True)
     print("Data Loaded")
 
     # Handle all model related stuff
     model = training_model()
     model = model.to(device)
+    params = model.parameters()
 
     if LOAD_PATH:
         model.load_state_dict(torch.load(LOAD_PATH))
 
     ##### PUT TASK SPECIFIC PRE-TRAINING THINGS HERE #####
-    all_dirs = get_model_dirs()
-    factorspeople = FactorsPeople(all_dirs)
+    # all_dirs = get_model_dirs()
+    # factorspeople = FactorsPeople(all_dirs)
 
-    static_factor_model = FactorsPeople(all_dirs)
-    static_factor_model.set_eval()
-    shading_albedo_loss = nn.MSELoss()
-    shading_lambda = 0.1
-    albedo_lambda = 0.1
+    # static_factor_model = FactorsPeople(all_dirs)
+    # static_factor_model.set_eval()
+    # shading_albedo_loss = nn.MSELoss()
+    # shading_lambda = 0.1
+    # albedo_lambda = 0.1
     ######################################################
 
     # Do the loss function / model thing for this too if needed
-    params = (
-        list(factorspeople.self_shading_net.parameters())
-        + list(factorspeople.shading_net.parameters())
-        + list(factorspeople.SH_model.parameters())
-        + list(factorspeople.albedo_net.parameters())
-        + list(factorspeople.shadow_net.parameters())
-        + list(factorspeople.refine_rendering_net.parameters())
-    )
+    # params = (
+    #     list(factorspeople.self_shading_net.parameters())
+    #     + list(factorspeople.shading_net.parameters())
+    #     + list(factorspeople.SH_model.parameters())
+    #     + list(factorspeople.albedo_net.parameters())
+    #     + list(factorspeople.shadow_net.parameters())
+    #     + list(factorspeople.refine_rendering_net.parameters())
+    # )
     optimizer = optim.Adam(params, lr=LR, betas=(0.9, 0.999))
 
     print("Model and auxillary components initialized")
@@ -152,76 +153,74 @@ if __name__ == "__main__":
 
             optimizer.zero_grad()
             #### PUT MODEL SPECIFIC FORWARD PASS CODE HERE ####
-            # try:
-            img, mask = factorspeople.get_image(
-                data["img_paths"].pop()[0], data["mask_paths"].pop()[0]
+            #### FOR SIGGRAPH TRAINING ####
+            # img, mask = factorspeople.get_image(
+            #     data["img_paths"].pop()[0], data["mask_paths"].pop()[0]
+            # )
+
+            # gt = img.detach() * mask.detach()
+            # out, factors = factorspeople.reconstruct(img, mask)
+            # _, static_factors = static_factor_model.reconstruct(img, mask)
+            # static_shading = static_factors["shading"]
+            # static_shading = static_shading / static_shading.max() * 255.0
+            # static_albedo = static_factors["albedo"]
+            # static_albedo = static_albedo / static_albedo.max() * 255.0
+
+            # shading = factors["shading"]
+            # shading = shading / shading.max() * 255.0
+
+            # albedo = factors["albedo"]
+            # albedo = albedo / albedo.max() * 255.0
+
+            # shading_loss = shading_albedo_loss(static_shading, shading) * shading_lambda
+            # albedo_loss = shading_albedo_loss(static_albedo, albedo) * albedo_lambda
+
+            ### FOR RELIGHTING HUMANS TRAINING ###
+            mask3 = (
+                Variable(
+                    torch.from_numpy(
+                        np.repeat(masks.detach().cpu().numpy(), 3, axis=0)
+                    ),
+                    requires_grad=True,
+                )
+                .squeeze(1)
+                .to(device)
             )
-            # except Exception:
-            #     continue
-
-            gt = img.detach() * mask.detach()
-            out, factors = factorspeople.reconstruct(img, mask)
-            _, static_factors = static_factor_model.reconstruct(img, mask)
-            static_shading = static_factors["shading"]
-            static_shading = static_shading / static_shading.max() * 255.0
-            static_albedo = static_factors["albedo"]
-            static_albedo = static_albedo / static_albedo.max() * 255.0
-
-            shading = factors["shading"]
-            shading = shading / shading.max() * 255.0
-
-            albedo = factors["albedo"]
-            albedo = albedo / albedo.max() * 255.0
-
-            shading_loss = shading_albedo_loss(static_shading, shading) * shading_lambda
-            albedo_loss = shading_albedo_loss(static_albedo, albedo) * albedo_lambda
-            # mask3 = (
-            #     Variable(
-            #         torch.from_numpy(
-            #             np.repeat(masks.detach().cpu().numpy(), 3, axis=0)
-            #         ),
-            #         requires_grad=True,
-            #     )
-            #     .squeeze(1)
-            #     .to(device)
-            # )
-            # mask9 = (
-            #     Variable(
-            #         torch.from_numpy(
-            #             np.repeat(masks.detach().cpu().numpy(), 9, axis=0)
-            #         ),
-            #         requires_grad=True,
-            #     )
-            #     .squeeze(1)
-            #     .to(device)
-            # )
+            mask9 = (
+                Variable(
+                    torch.from_numpy(
+                        np.repeat(masks.detach().cpu().numpy(), 9, axis=0)
+                    ),
+                    requires_grad=True,
+                )
+                .squeeze(1)
+                .to(device)
+            )
 
             # for running relighting humans
-            # gt = (images * mask3).to(device)
-            # images = 2.0 * images - 1
-            # model_input = (images * mask3).to(device)
+            gt = (images * mask3).to(device)
+            images = 2.0 * images - 1
+            model_input = (images * mask3).to(device)
             # imageio.imsave("model_input.png", model_input.detach().squeeze(0))
-            # rendering = model(gt)
-            # rendering_save = rendering.detach() - torch.min(rendering.detach())
-            # rendering_save = rendering_save / torch.max(rendering_save)
+            rendering = model(gt)
+            rendering_save = rendering.detach() - torch.min(rendering.detach())
+            rendering_save = rendering_save / torch.max(rendering_save)
 
-            # transport, albedo, light = model(model_input)
-            # transport = Variable((mask9 * transport).data[0], requires_grad=True)
-            # albedo = Variable((albedo * mask3).data[0], requires_grad=True)
-            # light = Variable(light.data, requires_grad=True)
-            # transport = transport.permute(1, 2, 0).to(device)
-            # albedo = albedo.permute(1, 2, 0).to(device)
-            # shading = torch.matmul(transport, light).to(device)
-            # rendering = (albedo * shading * 255.0).to(device)
+            transport, albedo, light = model(model_input)
+            transport = Variable((mask9 * transport).data[0], requires_grad=True)
+            albedo = Variable((albedo * mask3).data[0], requires_grad=True)
+            light = Variable(light.data, requires_grad=True)
+            transport = transport.permute(1, 2, 0).to(device)
+            albedo = albedo.permute(1, 2, 0).to(device)
+            shading = torch.matmul(transport, light).to(device)
+            rendering = (albedo * shading * 255.0).to(device)
             ####################################################
-
-            loss = criterion(out, gt) + shading_loss + albedo_loss
+            # add shading loss and albedo loss to this for the SIGGRAPH
+            loss = criterion(rendering, gt)
             running_loss += loss.item()
             loss.backward()
             optimizer.step()
-            wandb.log(
-                {"loss": loss, "shading_loss": shading_loss, "albedo_loss": albedo_loss}
-            )
+            wandb.log({"loss": loss})
 
         epoch_batch_loss = running_loss / len(train_loader)
         print(f"Loss: {epoch_batch_loss}")
@@ -229,13 +228,15 @@ if __name__ == "__main__":
     print("Training Finished")
 
     if SAVE_PATH:
-        # torch.save(model.state_dict(), SAVE_PATH)
-        torch.save(factorspeople.self_shading_net.state_dict(), SAVE_PATH + "ssn.pth")
-        torch.save(factorspeople.shading_net.state_dict(), SAVE_PATH + "sn.pth")
-        torch.save(factorspeople.SH_model.state_dict(), SAVE_PATH + "sh.pth")
-        torch.save(factorspeople.albedo_net.state_dict(), SAVE_PATH + "albedo.pth")
-        torch.save(factorspeople.shadow_net.state_dict(), SAVE_PATH + "shadow.pth")
-        torch.save(
-            factorspeople.refine_rendering_net.state_dict(), SAVE_PATH + "rrn.pth"
-        )
+        torch.save(model.state_dict(), SAVE_PATH + ".pth")
+
+        # SIGGRAPH Model
+        # torch.save(factorspeople.self_shading_net.state_dict(), SAVE_PATH + "ssn.pth")
+        # torch.save(factorspeople.shading_net.state_dict(), SAVE_PATH + "sn.pth")
+        # torch.save(factorspeople.SH_model.state_dict(), SAVE_PATH + "sh.pth")
+        # torch.save(factorspeople.albedo_net.state_dict(), SAVE_PATH + "albedo.pth")
+        # torch.save(factorspeople.shadow_net.state_dict(), SAVE_PATH + "shadow.pth")
+        # torch.save(
+        #     factorspeople.refine_rendering_net.state_dict(), SAVE_PATH + "rrn.pth"
+        # )
 
