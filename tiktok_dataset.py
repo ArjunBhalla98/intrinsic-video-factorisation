92d7f817086a65f432f43f832c82b504caa3f2bb
ArjunBhalla98 <arjunbhallasg@gmail.com>
2021-07-12 10:26:08 -0400

    debug

--- tiktok_dataset.py
+++ tiktok_dataset.py
@@ -61,8 +61,8 @@ class TikTokDataset(Dataset):
                 self.masks = []
                 self.names = []
 
-            video_imgs = sorted(glob.glob(f"{appended_path}/{folder}/images/*.png"))[1:]
-            video_masks = sorted(glob.glob(f"{appended_path}/{folder}/masks/*.png"))[1:]
+            video_imgs = sorted(glob.glob(f"{appended_path}/{folder}/images/*.png"))
+            video_masks = sorted(glob.glob(f"{appended_path}/{folder}/masks/*.png"))
 
             self.images += [
                 video_imgs[i : i + sample_size]
