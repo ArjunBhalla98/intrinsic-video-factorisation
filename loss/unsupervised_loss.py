808fa49acfa54ec4f97bf2fe58f8b0c4dcd242ab
ArjunBhalla98 <arjunbhallasg@gmail.com>
2021-07-12 10:04:19 -0400

    debug

--- loss/unsupervised_loss.py
+++ loss/unsupervised_loss.py
@@ -42,7 +42,8 @@ def optical_flow_loss(alb1, alb2, mask1, flow, device):
     alb1 = alb1.to(device)
     alb2 = alb2.to(device)
     mask1 = mask1.to(device)
-    alb1_predicted = warp_img(alb2, flow, device) * mask1
+    alb1_predicted = warp_img(alb2, flow, device)
+    # * mask1
     imageio.imsave(
         f"loss_pics/alb_{i}_predicted.png",
         alb1_predicted.squeeze(0).detach().cpu().permute(1, 2, 0).numpy(),
