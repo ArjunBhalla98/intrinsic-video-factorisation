83ca024f0d00b03bfb6873b5046b845bdcc8ac93
ArjunBhalla98 <arjunbhallasg@gmail.com>
2021-07-25 08:03:35 -0400

    debug

--- optical_flow/demo.py
+++ optical_flow/demo.py
@@ -63,8 +63,8 @@ def demo(args):
     model.eval()
 
     with torch.no_grad():
-        final_dict = {}
-        for i, folder in enumerate(sorted(os.listdir(args.path))):
+        for i, folder in enumerate(sorted(os.listdir(args.path))[:100]):
+            print(f"video {i+1}")
             images = glob.glob(
                 os.path.join(args.path + folder + "/images/", "*.png")
             ) + glob.glob(os.path.join(args.path + folder + "/images/", "*.jpg"))
@@ -73,11 +73,14 @@ def demo(args):
 
             ret = []
             is_ret = False
-            images = sorted(images)
+            images = sorted(images)[:100]
             mask_tmp_pth = "/phoenix/S3/ab2383/data/TikTok_dataset/00267/masks/0001.png"
             for imfile1, imfile2 in zip(images[:-1], images[1:]):
                 image1, _ = fact_people.get_image(imfile1, mask_tmp_pth)
                 image2, _ = fact_people.get_image(imfile2, mask_tmp_pth)
+
+                image1 = image1.to(DEVICE)
+                image2 = image2.to(DEVICE)
 
                 image1 *= 255.0
                 image2 *= 255.0
@@ -88,14 +91,9 @@ def demo(args):
                     ret = flow_up
                 else:
                     ret = torch.cat((ret, flow_up), 0)
-            final_dict[i + 1] = ret.detach().cpu().numpy()
-
-        with open("/phoenix/S3/ab2383/data/hundred_hundreds.pickle", "wb") as f:
-            pickle.dump(
-                final_dict,
-                f,
-                protocol=pickle.HIGHEST_PROTOCOL,
-            )
+            np.save(f"{i+1}.npy", ret.detach().cpu().numpy())
+            if i == 99:
+                break
 
 
 if __name__ == "__main__":
