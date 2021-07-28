9fbbe0cdf285d60d27eb51061a918e2227860602
ArjunBhalla98 <arjunbhallasg@gmail.com>
2021-07-28 16:46:44 -0400

    debug

--- viz.py
+++ viz.py
@@ -20,9 +20,9 @@ if __name__ == "__main__":
     fp = FactorsPeople(all_dirs, dev)
 
     img_path = "/phoenix/S3/ab2383/data/TikTok_dataset/00001/images/0001.png"
-    img2_path = "/phoenix/S3/ab2383/data/TikTok_dataset/00001/images/0002.png"
+    img2_path = "/phoenix/S3/ab2383/data/TikTok_dataset/00001/images/0004.png"
     mask_path = "/phoenix/S3/ab2383/data/TikTok_dataset/00001/masks/0001.png"
-    mask2_path = "/phoenix/S3/ab2383/data/TikTok_dataset/00001/masks/0002.png"
+    mask2_path = "/phoenix/S3/ab2383/data/TikTok_dataset/00001/masks/0004.png"
 
     img, mask = fp.get_image(img_path, mask_path)
     img2, mask2 = fp.get_image(img2_path, mask2_path)
@@ -41,7 +41,7 @@ if __name__ == "__main__":
     )
 
     flow = np.load("/phoenix/S3/ab2383/data/flows/1.npy")
-    flow = flow[1]
+    flow = flow[0]
     imageio.imsave("flowx.png", np.expand_dims(flow[0], 2))
     imageio.imsave("flowy.png", np.expand_dims(flow[1], 2))
     flowx, _ = fp.get_image("flowx.png", mask2_path)
